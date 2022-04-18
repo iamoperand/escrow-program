8ee1879728795151e1e68bb4f79eb3cb93c8b4ed
nikhil arora <narora200@gmail.com>
2022-04-18 12:50:14 +0530

    feat: escrow account ready

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,20 @@
+// ├─ src
+// │  ├─ lib.rs -> registering modules
+// │  ├─ entrypoint.rs -> entrypoint to the program
+// │  ├─ instruction.rs -> program API, (de)serializing instruction data
+// │  ├─ processor.rs -> program logic
+// │  ├─ state.rs -> program objects, (de)serializing state
+// │  ├─ error.rs -> program specific errors
+
+// The flow of a program using this structure looks like this:
+// - Someone calls the entrypoint
+// - The entrypoint forwards the arguments to the processor
+// - The processor asks instruction.rs to decode the instruction_data argument from the entrypoint function.
+// - Using the decoded data, the processor will now decide which processing function to use to process the request.
+// - The processor may use state.rs to encode state into or decode the state of an account which has been passed into the entrypoint.
+
 pub mod entrypoint;
+pub mod error;
+pub mod instruction;
+pub mod processor;
+pub mod state;
